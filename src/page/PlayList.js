cfdd0b79e1dbc668e5faf5a0530dd3bcffc44b28
John Doe <wangym@starutian.com>
2018-08-10 15:26:45 +0800

    secend

---
+++ src/page/PlayList.js
@@ -0,0 +1,14 @@
+import React, { Component }  from 'react';
+
+class PlayList extends Component {
+    componentDidMount(){
+        document.title = '歌单管理'
+    }
+    render(){
+        return(
+            <div>PlayList</div>
+        )
+    }
+}
+
+export default PlayList;
